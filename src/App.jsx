45a5dd47bc601d52743402f347fc2b6c54d8ea33
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-11-02 20:06:59 +0600

    Initial setup done.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Coffee perks website</h1>
+    </>
+  );
+}
+
+export default App;
